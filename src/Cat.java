ac2634557da8bdc672e8123ddac5939e1841fd34
Carter Gorling <rcgorling@mines.edu>
2024-10-02 23:20:36 -0600

    Centered the cat's eyes

--- src/Cat.java
+++ src/Cat.java
@@ -15,7 +15,7 @@ public class Cat {
 	private static final int HEAD_DIMENSION = 90;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
+	private static final int EYE_X = HEAD_DIMENSION/4+3;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 20;
